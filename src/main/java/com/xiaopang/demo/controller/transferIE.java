53e32bfed08cd629ae96b24d7f51b32cc0bff53f
Jimmy <253877916@qq.com>
2021-05-29 20:24:34 +0800

    updaet to restcontroller

--- src/main/java/com/xiaopang/demo/controller/transferIE.java
+++ src/main/java/com/xiaopang/demo/controller/transferIE.java
@@ -6,15 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class transferIE {
 
     @Autowired
     private TransferSerivce transferSerivce;
 
     @GetMapping("/OpenIE")
-    @ResponseBody
     public Boolean transfer( String url){
         return transferSerivce.doTransfer(url);
     }
